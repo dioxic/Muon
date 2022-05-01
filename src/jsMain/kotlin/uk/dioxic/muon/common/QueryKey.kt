114afb147c17f7287732c9fa224a3be31260b205
Mark Baker-Munton <dioxic@gmail.com>
2022-05-01 10:31:20 +0100

    fix: invalidate library queries on index refresh

--- src/jsMain/kotlin/uk/dioxic/muon/common/QueryKey.kt
+++ src/jsMain/kotlin/uk/dioxic/muon/common/QueryKey.kt
@@ -3,7 +3,8 @@ package uk.dioxic.muon.common
 import react.query.QueryKey
 
 object QueryKey {
-    fun trackSearch(text: String) = QueryKey<QueryKey>("track", text)
+    fun trackSearch(text: String) = QueryKey<QueryKey>("library", text)
+    val LIBRARY = QueryKey<QueryKey>("library")
     val SETTINGS = QueryKey<QueryKey>("settings")
     val IMPORT = QueryKey<QueryKey>("import")
 }
